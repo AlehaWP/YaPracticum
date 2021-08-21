94c2991486318c26e19fdec760cf354fb09963c8
AlehaWP <nerchenko@gmail.com>
2021-08-22 00:24:43 +0300

    Clean Version

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,32 +7,44 @@ import (
 	"crypto/md5"
 )
 
-var Urls map[string]string // =  make(map[int]string)
+var Urls map[string]string
 
 func MD5(data []byte) string {
 	h := md5.Sum(data)
 	return fmt.Sprintf("%x", h)
 }
 
+func handlerPost (w http.ResponseWriter, r *http.Request){
+	textBody, err := io.ReadAll(r.Body)
+	if err != nil {
+
+	}
+	defer r.Body.Close()
+	mdUrl := MD5(textBody)
+	Urls[mdUrl] = string(textBody)
+	w.WriteHeader(201)
+	io.WriteString (w, mdUrl)
+}
+
+func handlerGet(w http.ResponseWriter, r *http.Request){
+	id := r.URL.Path[1:]
+	if val, ok := Urls[id]; ok {
+		w.Header().Add("Location", val)
+		w.WriteHeader(307)
+	} else {
+		w.WriteHeader(500)
+	}
+	w.Write([]byte(id))
+}
+
 func handlerFunction (w http.ResponseWriter, r *http.Request) {
-	url := r.URL.String()
 	switch r.Method{
 		case  http.MethodPost:
-			textBody, err := io.ReadAll(r.Body)
-			if err != nil {
-
-			}
-			defer r.Body.Close()
-
-			url = string(textBody)
-			mdUrl := MD5(textBody)
-			Urls[mdUrl] = url
-			io.WriteString (w, mdUrl)
+			handlerPost(w, r)
 		case http.MethodGet:
-			id := r.URL.Path[1:]
-			io.WriteString (w, Urls[id])
+			handlerGet(w, r)
 	    default:
-			io.WriteString (w, "default")
+			w.WriteHeader(404)
 	}
 }
 
