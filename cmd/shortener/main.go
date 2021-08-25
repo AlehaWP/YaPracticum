88955958abe6db407e5f3502efb844dc9e60aee4
AlehaWP <nerchenko@gmail.com>
2021-08-25 17:58:01 +0300

    Привожу в порядок код

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,58 +1,103 @@
 package main
 
 import (
-	"io"
-	"fmt"
-	"net/http"
+	"context"
 	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
 )
 
-var Urls map[string]string
+type Url []byte
 
-func MD5(data []byte) string {
+func (u Url) String() string {
+	return string(u)
+}
+
+type Urls map[string]Url
+
+func (u Urls) SaveURL(url Url) string {
+	r := u.MD5(url)
+	u[r] = url
+	return r
+}
+
+func (u Urls) GetURL(id string) string {
+	return u[id].String()
+}
+
+func (u Urls) MD5(data Url) string {
 	h := md5.Sum(data)
 	return fmt.Sprintf("%x", h)
 }
 
-func handlerPost (w http.ResponseWriter, r *http.Request){
-	textBody,_:= io.ReadAll(r.Body)
+var urlsData Urls
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	textBody, _ := io.ReadAll(r.Body)
 	defer r.Body.Close()
-	mdURL := MD5(textBody)
-	Urls[mdURL] = string(textBody)
+
+	retUrl := "http://" + r.Host + "/" + urlsData.SaveURL(textBody)
 	w.WriteHeader(201)
-	io.WriteString (w, "http://" + r.Host + "/" + mdURL)
+	io.WriteString(w, retUrl)
 }
 
-func handlerGet(w http.ResponseWriter, r *http.Request){
+func handlerGet(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[1:]
-	if val, ok := Urls[id]; ok {
-		w.Header().Add("Location", val)
+	if val, ok := urlsData[id]; ok {
+		w.Header().Add("Location", GetURL(id))
 		w.WriteHeader(307)
 	} else {
 		w.WriteHeader(400)
 	}
-	w.Write([]byte(id))
 }
 
-func handlerFunction (w http.ResponseWriter, r *http.Request) {
-	switch r.Method{
-		case  http.MethodPost:
-			handlerPost(w, r)
-		case http.MethodGet:
-			handlerGet(w, r)
-	    default:
-			w.WriteHeader(400)
+func handlerFunction(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		handlerPost(w, r)
+	case http.MethodGet:
+		handlerGet(w, r)
+	default:
+		w.WriteHeader(400)
 	}
 }
 
-
-func main() {
-	Urls =  make(map[string]string)
+func startServer() *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlerFunction)
-	server := &http.Server{
-        Addr: "localhost:8080",
+	s := &http.Server{
+		Addr:    "localhost:8080",
 		Handler: mux,
-    }
-	server.ListenAndServe()
+	}
+	go func() {
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+	return s
+}
+
+func scanQuit(ch chan bool) {
+	var inputText string
+	for strings.ToLower(inputText) != "quit" {
+		fmt.Println("For server stop please input: quit")
+		fmt.Scanf("%s\n", &inputText)
+	}
+	ch <- true
+}
+
+func main() {
+	urlsData = make(map[string]Url)
+	server := startServer()
+
+	sigQuitChan := make(chan bool)
+	go scanQuit(sigQuitChan)
+	<-sigQuitChan
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Printf("HTTP server Shutdown: %v", err)
+	}
 }
