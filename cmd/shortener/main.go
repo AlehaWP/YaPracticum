c8cf37c96d66e22d70e26a07d5a60c419c0937be
AlehaWP <nerchenko@gmail.com>
2021-08-29 13:06:43 +0300

    Сделан роутер

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,47 +11,47 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type Url []byte
-
-func (u Url) String() string {
-	return string(u)
-}
-
-func (u Url) MD5() string {
-	h := md5.Sum(u)
-	return fmt.Sprintf("%x", h)
-}
 
 type Repository interface {
-	GetURL(string) (Url, bool)
-	SaveURL(Url) string
+	GetURL(string) (string, bool)
+	SaveURL([]byte) string
 }
 
-type UrlsData map[string]Url
+type UrlsData map[string][]byte
 
-func (u *UrlsData) SaveURL(url Url) string {
-	r := url.MD5()
+func (u *UrlsData) SaveURL(url []byte) string {
+	r := MD5(url)
 	(*u)[r] = url
 	return r
 }
 
-func (u *UrlsData) GetURL(id string) (Url, bool) {
+func (u *UrlsData) GetURL(id string) (string, bool) {
 	if r, ok := (*u)[id]; ok {
-		return r, true
+		return string(r), true
 	}
-	return nil, false
+	return "", false
+}
+
+
+func MD5(b []byte) string {
+	h := md5.Sum(b)
+	return fmt.Sprintf("%x", h)
 }
 
 type Server struct {
 	http.Server
 }
 
-func (s *Server) start(addr string, router func(http.ResponseWriter, *http.Request)) {
+func (s *Server) start(addr string, repo Repository) {
 	r := chi.NewRouter()
-	r.Get("/{id}", router)
-	r.Post("/", router)
+	r.Post("/", handlerUrlPost(repo))
+	r.Route("/{id}", func(r chi.Router){
+		r.Use(UrlCtx)
+		r.Get("/", handlerUrlGet(repo))
+	})
+	// r.Get("/{id}", handlerUrlGet(repo))
 	s.Addr = addr
-	s.Handler = r//http.HandlerFunc(router)
+	s.Handler = r
 	go func() {
 		if err := s.ListenAndServe(); err != http.ErrServerClosed {
 			log.Fatalf("HTTP server ListenAndServe: %v", err)
@@ -74,35 +74,36 @@ func (s *Server) stop() {
 	}
 }
 
-func Router(repo Repository) http.HandlerFunc {
+
+
+func handlerUrlPost(repo Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			handlerPost(w, r, repo)
-		case http.MethodGet:
-			handlerGet(w, r, repo)
-		default:
-			w.WriteHeader(400)
-		}
+		textBody, _ := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		retUrl := "http://" + r.Host + "/" + repo.SaveURL(textBody)
+		w.WriteHeader(201)
+		io.WriteString(w, retUrl)
 	}
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request, repo Repository) {
-	textBody, _ := io.ReadAll(r.Body)
-	defer r.Body.Close()
-    fmt.Println(string(textBody))
-	retUrl := "http://" + r.Host + "/" + repo.SaveURL(Url(textBody))
-	w.WriteHeader(201)
-	io.WriteString(w, retUrl)
+func UrlCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	  ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
+	  next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
-func handlerGet(w http.ResponseWriter, r *http.Request, repo Repository) {
-	id := r.URL.Path[1:]
-	if val, ok := repo.GetURL(id); ok {
-		w.Header().Add("Location", val.String())
-		w.WriteHeader(307)
-	} else {
-		w.WriteHeader(400)
+func handlerUrlGet(repo Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		id := ctx.Value("id").(string)
+		if val, ok := repo.GetURL(id); ok {
+			log.Println(val)
+			w.Header().Add("Location", val)
+			w.WriteHeader(307)
+		} else {
+			w.WriteHeader(400)
+		}
 	}
 }
 
@@ -118,6 +119,6 @@ func scanQuit(ch chan bool) {
 func main() {
 	s := new(Server)
 	urlData := make(UrlsData)
-	s.start("localhost:8080", Router(&urlData))
+	s.start("localhost:8080", &urlData)
 	s.listenChanToQuit(scanQuit)
 }
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"io"
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -15,23 +15,42 @@ type UrlsMock struct {
 	mock.Mock
 }
 
-func (m UrlsMock) SaveURL(url Url) string {
+func (m UrlsMock) SaveURL(url []byte) string {
 	args := m.Called(url)
 	return args.String(0)
 }
 
-func (m UrlsMock) GetURL(id string) (Url, bool) {
+func (m UrlsMock) GetURL(id string) (string, bool) {
 	args := m.Called(id)
-	return args.Get(0).(Url), args.Bool(1)
+	return args.String(0), args.Bool(1)
 }
 
-func TestRouter(t *testing.T) {
+func TestHandlerUrlGet(t *testing.T) {
 	repoMock := UrlsMock{}
-	repoMock.On("GetURL", "123123asdasd").Return(Url("www.example.com"), true)
-	repoMock.On("GetURL", "123123").Return(Url(""), false)
-	repoMock.On("SaveURL", Url("www.example.com")).Return("123123asdasd")
+	repoMock.On("GetURL", "123123asdasd").Return("www.example.com", true)
+	repoMock.On("GetURL", "123123").Return("", false)
 
-	handler := http.HandlerFunc(Router(repoMock))
+	handler := http.HandlerFunc(handlerUrlGet(repoMock))
+
+	r := httptest.NewRequest("GET", "/123123asdasd", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	ctx := context.WithValue(context.Background() , "id", "123123asdasd")
+	handler.ServeHTTP(w, r.WithContext(ctx))
+	assert.Equal(t, 307, w.Result().StatusCode, "Не верный код ответа GET")
+	assert.Equal(t, w.Header().Get("Location"), "www.example.com", "Не верный ответ GET")
+
+	r = httptest.NewRequest("GET", "/123123", strings.NewReader(""))
+	w = httptest.NewRecorder()
+	ctx = context.WithValue(context.Background() , "id", "123123")
+	handler.ServeHTTP(w, r.WithContext(ctx))
+	assert.Equal(t, 400, w.Result().StatusCode, "Не верный код ответа GET")
+}
+
+
+func TestHandlerUrlPost(t *testing.T) {
+	repoMock := UrlsMock{}
+	repoMock.On("SaveURL", []byte("www.example.com")).Return("123123asdasd")
+	handler := http.HandlerFunc(handlerUrlPost(repoMock))
 	r := httptest.NewRequest("POST", "http://localhost:8082/", strings.NewReader("www.example.com"))
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, r)
@@ -40,15 +59,4 @@ func TestRouter(t *testing.T) {
 	defer res.Body.Close()
 	assert.Equal(t, 201, w.Result().StatusCode, "Не верный код ответа POST")
 	assert.Equal(t, "http://localhost:8082/123123asdasd", string(b), "Не верный ответ POST")
-
-	r = httptest.NewRequest("GET", "/123123asdasd", strings.NewReader(""))
-	w = httptest.NewRecorder()
-	handler.ServeHTTP(w, r)
-	assert.Equal(t, 307, w.Result().StatusCode, "Не верный код ответа GET")
-	assert.Equal(t, w.Header().Get("Location"), "www.example.com", "Не верный ответ GET")
-
-	r = httptest.NewRequest("GET", "/123123", strings.NewReader(""))
-	w = httptest.NewRecorder()
-	handler.ServeHTTP(w, r)
-	assert.Equal(t, 400, w.Result().StatusCode, "Не верный код ответа GET")
 }
