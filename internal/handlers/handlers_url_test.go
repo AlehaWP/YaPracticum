8598692a9b7adc94f6c590b46dc9e12d9ed21c00
AlehaWP <nerchenko@gmail.com>
2021-08-31 00:54:00 +0300

    Перенес тесты

--- internal/handlers/handlers_url_test.go
+++ internal/handlers/handlers_url_test.go
@@ -1,13 +1,14 @@
-package main
+package handlers
 
 import (
-	"io"
 	"context"
+	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"log"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -29,42 +30,40 @@ func (m UrlsMock) GetURL(id string) (string, bool) {
 func TestHandlerUrlGet(t *testing.T) {
 	dataTests := map[string]map[string]interface{}{
 		"test1": {
-			"reqID"      : "123123asdasd",
-			"result"     : "www.example.com",
-			"resStatus"  : 307,
+			"reqID":       "123123asdasd",
+			"result":      "www.example.com",
+			"resStatus":   307,
 			"mockReturn1": "www.example.com",
 			"mockReturn2": true,
-
 		},
 		"test2": {
-			"reqID"      : "123123",
-			"result"     : "",
-			"resStatus"  : 400,
+			"reqID":       "123123",
+			"result":      "",
+			"resStatus":   400,
 			"mockReturn1": "",
 			"mockReturn2": false,
 		},
 	}
 
 	repoMock := new(UrlsMock)
-	handler := http.HandlerFunc(handlerUrlGet(repoMock))
+	handler := http.HandlerFunc(HandlerUrlGet(repoMock))
 
 	for key, value := range dataTests {
 		log.Println("start test", key)
 		repoMock.On("GetURL", value["reqID"].(string)).Return(value["mockReturn1"].(string), value["mockReturn2"].(bool))
 		r := httptest.NewRequest("GET", "/"+value["reqID"].(string), strings.NewReader(""))
 		w := httptest.NewRecorder()
-		ctx := context.WithValue(context.Background() , "id", value["reqID"].(string))
+		ctx := context.WithValue(context.Background(), "id", value["reqID"].(string))
 		handler.ServeHTTP(w, r.WithContext(ctx))
 		assert.Equal(t, value["resStatus"].(int), w.Result().StatusCode, "Не верный код ответа GET")
 		assert.Equal(t, w.Header().Get("Location"), value["result"].(string), "Не верный ответ GET")
 	}
 }
 
-
 func TestHandlerUrlPost(t *testing.T) {
 	repoMock := UrlsMock{}
 	repoMock.On("SaveURL", []byte("www.example.com")).Return("123123asdasd")
-	handler := http.HandlerFunc(handlerUrlPost(repoMock))
+	handler := http.HandlerFunc(HandlerUrlPost(repoMock))
 	r := httptest.NewRequest("POST", "http://localhost:8082/", strings.NewReader("www.example.com"))
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, r)
