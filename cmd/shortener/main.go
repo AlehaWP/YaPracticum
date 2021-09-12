61445c91e1d872a5e3c96d1c4643044f391d53a8
AlehaWP <nerchenko@gmail.com>
2021-09-13 00:47:11 +0300

    df

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/AlehaWP/YaPracticum.git/internal/server"
 )
 
-// Main.
+// Main.git
 func main() {
 	urlRepo := make(repository.URLRepo)
 	serialize.ReadURLSFromFile(&urlRepo)
