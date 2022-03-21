2fa68082b1006b2c129ad6ca901c70fd96ef9b8a
AlehaWP <nerchenko@gmail.com>
2022-03-21 23:07:18 +0300

    Первый линте

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+)
+
+func main() {
+	fmt.Println("Старт проверки")
+	multichecker.Main(
+		printf.Analyzer,
+		shadow.Analyzer,
+		structtag.Analyzer,
+	)
+}
