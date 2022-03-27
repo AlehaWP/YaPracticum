1d6923625e3e7728f5c0776c892c31a7c0834068
AlehaWP <nerchenko@gmail.com>
2022-03-28 01:04:22 +0300

    Добавил флаги

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,8 +11,15 @@ import (
 	"github.com/AlehaWP/YaPracticum.git/internal/signal"
 )
 
+var (
+	BuildVersion string = "N/A"
+	BuildDate    string = "N/A"
+	BuildCommit  string = "N/A"
+)
+
 // Main.
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", BuildVersion, BuildDate, BuildCommit)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
